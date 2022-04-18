3de701c7e80ffd6d28e4efa7117b8e15477df84f
eknag <eknag@andrew.cmu.edu>
2022-04-18 13:54:27 -0400

    added other datasets

--- scripts/train.py
+++ scripts/train.py
@@ -11,6 +11,23 @@ import pythae.trainers as trainers
 import torch
 import yaml
 from torchvision import datasets
+import torchvision
+import os
+
+
+def load_image_folder(folder_path: str) -> torch.Tensor:
+    """
+    Loads an image folder containing jpg images of the same size.
+    """
+    data = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".jpg"):
+            img = Image.open(os.path.join(folder_path, filename))
+            data.append(img)
+
+    data = torch.stack(data)
+
+    return data
 
 
 def get_dataset(dataset_name: str, config) -> Tuple[Union[torch.Tensor, np.ndarray]]:
@@ -21,15 +38,23 @@ def get_dataset(dataset_name: str, config) -> Tuple[Union[torch.Tensor, np.ndarr
 
     if dataset_name == "CIFAR10":
         dataset = datasets.CIFAR10(root=config["data_dir"], train=True, download=True,)
-        data = dataset.data
-        del dataset
-
-        data = torch.tensor(data).to(device)
-
+    elif dataset_name == "CIFAR100":
+        dataset = datasets.CIFAR100(root=config["data_dir"], train=True, download=True,)
+    elif dataset_name == "MNIST":
+        dataset = datasets.MNIST(root=config["data_dir"], train=True, download=True,)
+    elif dataset_name == "CELEBA":
+        # load the dataset directly to a tensor
+        dataset = load_image_folder(
+            os.path.join(config["data_dir"], "img_align_celeba")
+        )
+        assert dataset.shape[1:] == (3, 64, 64)
     else:
         raise ValueError(f"The dataset {dataset_name} is not implemented.")
 
-    assert isinstance(data, torch.Tensor)
+    data = dataset.data
+    del dataset
+
+    data = torch.tensor(data).to(device)
 
     # Correct shape of single channel images
     if len(data.shape) == 3:
