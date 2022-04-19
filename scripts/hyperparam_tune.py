7438fa918a251284bc80f3c0024ce7e0cdd1bc57
everett <everett@agot.ai>
2022-04-19 16:58:31 -0400

    added hyperparam tuning, works now

--- scripts/hyperparam_tune.py
+++ scripts/hyperparam_tune.py
@@ -6,6 +6,7 @@ from ray.tune.schedulers import ASHAScheduler
 from ray.tune.suggest.hyperopt import HyperOptSearch
 from hyperopt import hp
 from train import train
+import ray
 
 if __name__ == "__main__":
 
@@ -15,20 +16,22 @@ if __name__ == "__main__":
 
     config = yaml.safe_load(open(args.base_config, "r"))
 
-    lr = hp.loguniform("lr", 1e-6, 1e-2)
-    latent_dim = hp.choice("latent_dim", [16, 50, 256])
-    gamma = hp.choice("gamma", [0.1, 0.001])
+    lr = tune.loguniform(1e-6, 1e-2)
+    batch_size = tune.choice([64, 256])
+    # gamma = hp.choice("gamma", [0.1, 0.001])
 
     config["training_config"]["learning_rate"] = lr
-    config["model_config"]["latent_dim"] = latent_dim
-    config["model_config"]["gamma"] = gamma
+    config["training_config"]["batch_size"] = batch_size
 
-    hyperopt_search = HyperOptSearch(config, metric="eval_loss", mode="min")
+    ray.init(log_to_driver=False)
 
     analysis = tune.run(
         train,
-        num_samples=1,
-        search_alg=hyperopt_search,
-        resources_per_trial={"gpu": 1},
+        config=config,
+        num_samples=10,
+        resources_per_trial={"gpu": 1, "cpu": 8},
         raise_on_failed_trial=False,
+        scheduler=ASHAScheduler(metric="eval_loss", mode="min"),
+        local_dir="./ray_results",
+        name="crvae_simple_training_params",
     )
