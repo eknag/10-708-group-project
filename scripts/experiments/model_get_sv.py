3c7d4d59aaff52d399c2bbde6f808be283cfaa0c
Andrew Stange <andrew.r.stange@gmail.com>
2022-04-19 14:52:57 -0400

    enable gpu

--- scripts/experiments/model_get_sv.py
+++ scripts/experiments/model_get_sv.py
@@ -15,13 +15,12 @@ def spec_mnist(self, input, output):
         s, u, v = k_generic_power_method(self.forward, self.input_sizes[0],
                 n_sv,
                 max_iter=500,
-                use_cuda=False)
+                use_cuda=torch.cuda.is_available())
         self.spectral_norm = s
         self.u = u
         self.v = v
 
 def save_singular(model, dest_dir):
-    # TODO(as) be able to load and save singular values
     layer_names = Counter()
     for layer in model.layers:
         layer_name  = layer._get_name() + "_" + str(layer_names[layer._get_name()])
@@ -31,6 +30,15 @@ def save_singular(model, dest_dir):
         torch.save(layer.u, open(output_name + "_left_singular", 'wb'))
         torch.save(layer.v, open(output_name + "_left_singular", 'wb'))
 
+def load_singular(model, dest_dir):
+    layer_names = Counter()
+    for layer in model.layers:
+        layer_name  = layer._get_name() + "_" + str(layer_names[layer._get_name()])
+        layer_names[layer._get_name()] += 1
+        output_name = dest_dir + "/" + model._get_name() + "_" + layer_name
+        torch.load(layer.spectral_norm, open(output_name + "_spectral", 'wb'))
+        torch.load(layer.u, open(output_name + "_left_singular", 'wb'))
+        torch.load(layer.v, open(output_name + "_left_singular", 'wb'))
 
 
 
