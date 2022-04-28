fca480e0c3190621b0c98d05fd5d4d447152152d
eknag <61752493+eknag@users.noreply.github.com>
2022-04-28 15:10:02 -0400

    Fix/update eval (#23)
    * updated eval script, added is and support for celeba
    
    * Updating conda environment
    
    * Updating conda environment
    
    * fixed utils function error
    
    Co-authored-by: everett <everett@agot.ai>
    Co-authored-by: as821 <49156741+as821@users.noreply.github.com>

---
+++ scripts/utils.py
@@ -0,0 +1,33 @@
+import os
+
+def create_sample_mosaic(sampler, n_rows, n_cols, fname):
+    """
+    Create a sample mosaic of the given sampler.
+
+    Parameters
+    ----------
+    sampler : Sampler
+        Sampler to create the mosaic of.
+    n_rows : int
+        Number of rows in the mosaic.
+    n_cols : int
+        Number of columns in the mosaic.
+    fname : str
+        Name of the file to save the mosaic to.
+    """
+    import matplotlib.pyplot as plt
+    from matplotlib.gridspec import GridSpec
+
+    fig = plt.figure(figsize=(8, 8))
+    gs = GridSpec(n_rows, n_cols)
+
+    for i in range(n_rows):
+        for j in range(n_cols):
+            ax = plt.subplot(gs[i, j])
+            ax.imshow(sampler.sample(1)[0].cpu().numpy().transpose(1, 2, 0))
+            ax.axis('off')
+    print(f"saving figure to {fname + '.png'}")
+    path = '/'.join(fname.split('/')[:-1])
+    if not os.path.exists(path):
+        os.makedirs(path)
+    plt.savefig(fname + '.png')
